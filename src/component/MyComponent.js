8f84e347f50849b961b79a62abb725ce85664d25
Ngo Gia Bao <giabaongo1@gmail.com>
2025-01-12 19:42:50 +0700

    component cha con

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -2,57 +2,19 @@
 // function component
 
 import React from 'react';
+import UserInfo from './UserInfo2';
 
 class MyComponent extends React.Component {
 
-    state = {
-        name: 'Bao',
-        address: 'HCM',
-        age: 20
-    };
 
-    handleClick(event) {
-        console.log(">> click me by button")
-        console.log("My name is", this.state.name)
-
-        this.setState({
-            name: 'Ngo'
-        })
-
-        this.setState({
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-    }
-
-    handleOnMouseOver(event) {
-        console.log(event)
-    }
-
-    handleOnChangeInput(event) {
-        this.setState({
-            name: event.target.value
-        })
-        // console.log(event,event.target.value)
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state)
-        alert(this.state.name)
-    }
 
     // jsx (cho phep viet code js trong html)
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm {this.state.age}
-                <button onClick={(event) => { this.handleClick(event) }}>click me</button>
-                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
-                    <input
-                        type='text'
-                        onChange={(event) => { this.handleOnChangeInput(event) }}/>
-                    <button>submit</button>
-                </form>
+                hefdsf
+
+                <UserInfo />
             </div>
         );
     }
