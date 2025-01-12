d21568a90b2fa4869c20bea8e8ac72d59381defe
Ngo Gia Bao <giabaongo1@gmail.com>
2025-01-12 23:27:34 +0700

    conditional output

--- src/component/DisplayInfor.js
+++ src/component/DisplayInfor.js
@@ -2,28 +2,51 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
 
+    state = {
+        isShowListUser: true
+    }
+
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
 
     render() {
 
-        const {listUser} = this.props;
+        const { listUser } = this.props;
 
-        console.log(this.props)
+        console.log(listUser)
         //props => properties (tai san) dung de chuyen du lieu
         //  tu component cha sang component con
         return (
             <div>
-                {listUser.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div>My name's {user.name}</div>
-                            <div>My age's {user.age}</div>
-                            <hr/>
-                        </div>
-                    )
-                })}
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                        
+                        {this.state.isShowListUser ===true ? "Hide list users" : "Show list users"}
+                        </span>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUser.map((user) => {
+                            console.log(user)
 
-                {/* <div>My name's {this.props.name}</div>
+                            return (
+                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                    <div>My name's {user.name}</div>
+                                    <div>My age's {user.age}</div>
+                                    <hr />
+                                </div>
+                            )
+
+
+                        })}
+
+                        {/* <div>My name's {this.props.name}</div>
                 <div>My age's {this.props.age}</div> */}
+                    </div>
+                }
             </div>
         )
     }
