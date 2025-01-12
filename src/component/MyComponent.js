79b5ab80409fe11436b855a2eb9dd745f470af75
Ngo Gia Bao <giabaongo1@gmail.com>
2025-01-12 21:53:12 +0700

    component cha con

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -12,7 +12,7 @@ class MyComponent extends React.Component {
     render() {
         return (
             <div>
-                hefdsf
+                hello
 
                 <UserInfo />
             </div>
--- src/component/UserInfo2.js
+++ src/component/UserInfo2.js
@@ -15,6 +15,12 @@ class UserInfo extends React.Component {
         // console.log(event,event.target.value)
     }
 
+    handleOnChangeAge(event) {
+        this.setState({
+            age: event.target.value
+        })
+    }
+
     handleOnSubmit = (event) => {
         event.preventDefault();
         console.log(this.state)
@@ -24,11 +30,17 @@ class UserInfo extends React.Component {
         return (
             <div>
                 My name is {this.state.name} and I'm {this.state.age}
-                <button onClick={(event) => { this.handleClick(event) }}>click me</button>
                 <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+                    <label>Name: </label>
                     <input
+                        value={this.state.name}
                         type='text'
                         onChange={(event) => { this.handleOnChangeInput(event) }} />
+                    <label>Age: </label>
+                    <input
+                        value={this.state.age}
+                        type='text'
+                        onChange={(event) => { this.handleOnChangeAge(event) }} />
                     <button>submit</button>
                 </form>
             </div>
