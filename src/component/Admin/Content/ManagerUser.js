5bf4de1ecac06d94b65214620b89bdd89691f766
Ngo Gia Bao <giabaongo1@gmail.com>
2025-02-07 22:51:58 +0700

    Model add new user

--- src/component/Admin/Content/ManagerUser.js
+++ src/component/Admin/Content/ManagerUser.js
@@ -1,17 +1,19 @@
 import ModelCreateUser from "./ModelCreateUser";
-
+import './ManageUser.scss';
 const ManagerUser = (props) => {
     return (
         <div className="manager-user-container">
             <div className="title">
                 Manager User
             </div>
-            <div className="users-content"></div>
-            <div>
-                <button className="btn-add-user">Add new User</button>
-            </div>
-            <div>
-                table user
+            <div className="users-content">
+                <div>
+                    <button className="btn-add-user">Add new User</button>
+                </div>
+                <div>
+                    table user
+
+                </div>
                 <ModelCreateUser />
             </div>
         </div>
--- src/component/Admin/Content/ModelCreateUser.js
+++ src/component/Admin/Content/ModelCreateUser.js
@@ -1,14 +1,30 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
+import img1 from '../../../assets/bg2.jpg';
+import { FcPlus } from 'react-icons/fc';
 const ModelCreateUser = () => {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [role, setRole] = useState("USER");
+    const [img, setImg] = useState('');
+    const [previewImage, setPreviewImage] = useState('');
 
+    const handleUploadFile = (e) => {
+        if (e.target && e.target.files && e.target.files[0]) {
+            setPreviewImage(URL.createObjectURL(e.target.files[0]));
+            setImg(e.target.files[0]);
+        } //else setPreviewImage('');
+
+
+        console.log(e.target.files[0]);
+    }
 
     return (
         <>
@@ -21,6 +37,7 @@ const ModelCreateUser = () => {
                 onHide={handleClose}
                 size='lg'
                 backdrop="static"
+                className='modal-add-user'
             >
                 <Modal.Header closeButton>
                     <Modal.Title>Add new user</Modal.Title>
@@ -29,29 +46,55 @@ const ModelCreateUser = () => {
                     <form className="row g-3">
                         <div className="col-md-6">
                             <label className="form-label">Email</label>
-                            <input type="email" className="form-control" id="inputEmail4" />
+                            <input
+                                type="email"
+                                className="form-control"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">Password</label>
-                            <input type="password" className="form-control" id="inputPassword4" />
+                            <input
+                                type="password"
+                                className="form-control"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </div>
 
                         <div className="col-md-6">
                             <label className="form-label">Username</label>
-                            <input type="text" className="form-control" id="inputCity" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={username}
+                                onChange={(event) => setUsername(event.target.value)} />
                         </div>
                         <div className="col-md-4">
                             <label className="form-label">Role</label>
-                            <select id="inputState" className="form-select">
-                                <option selected value="USER">User</option>
+                            <select
+                                className="form-select"
+                                value={role}
+                                onChange={(e) => setRole(e.target.value)}
+                            >
+                                <option value="USER">User</option>
                                 <option value="ADMIN">Admin</option>
                             </select>
                         </div>
                         <div className='col-md-12'>
-                            <label className='form-label'>Image</label>
-                            <input type="file" />
+                            <label className='form-label label-upload' htmlFor='labelUpload'><FcPlus /> Upload File Image</label>
+                            <input
+                                type="file"
+                                id='labelUpload' hidden
+                                onChange={(e) => handleUploadFile(e)} />
                         </div>
 
+                        <div className='col-md-12 img-preview'>
+                            {/* <span>Preview Image</span> */}
+
+                            {previewImage ? <img src={previewImage} /> : <span>Preview Image</span>}
+                        </div>
 
                     </form>
 
